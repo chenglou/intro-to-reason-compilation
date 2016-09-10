86f8dcf1e3b2fd1f769d3800ba9277ff1964adda
abby <AbbyHE@users.noreply.github.com>
2016-09-09 22:25:00 -0700

    Update run.sh

--- step3/run.sh
+++ step3/run.sh
@@ -6,7 +6,7 @@
 
 rm -f src/*.cm*
 
-# Introducing `ocamldep`! It's an utility that ships with OCaml, which analyses
+# Introducing `ocamldep`! It's a utility that ships with OCaml, which analyses
 # the files and outputs them in the order of dependency (aka, a topological
 # sort). If A depends on B then A will certainly come after B.
 
